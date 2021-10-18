7a31a24a65c422c2f721714b9b2aa1921608d9eb
Sunny <73135987+wzhao0842@users.noreply.github.com>
2021-10-17 22:25:25 -0700

    Update Connected-Components.cpp

--- Connected-Components.cpp
+++ Connected-Components.cpp
@@ -35,3 +35,54 @@ int main(){
         printf("%d %d\n", Unconnected[i-1], Unconnected[i]);
     }
 }
+
+//Moocast
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct cow{
+    int x, y, dis;
+};
+
+void dfs(vector<int> adj[], vector<bool>& visited, int node){
+    visited[node] = 1;
+    for(auto u:adj[node]){
+        if(!visited[u]){
+            dfs(adj, visited, u);
+        }
+    }
+}
+
+int main(){
+    int n; scanf("%d", &n);
+    vector<cow> cows(n+5);
+    vector<int> adj[n+5];
+    vector<bool> visited(n+5, 0);
+    for(int i=0;i<n;i++){
+        scanf("%d%d%d", &cows[i].x, &cows[i].y, &cows[i].dis);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            //any decimal need to increment the value by 1
+            double preDis = (double)sqrt(pow(cows[i].x-cows[j].x, 2) + pow(cows[i].y-cows[j].y, 2));
+            int realDis = preDis; 
+            if(realDis != preDis){
+                realDis += 1; 
+            }
+            if(realDis <= cows[i].dis){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+    }
+
+    dfs(adj, visited, 0);
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        if(visited[i]){
+            cnt++;
+        }
+    }
+    printf("%d\n", cnt);
+}
